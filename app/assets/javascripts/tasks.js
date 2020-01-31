97770ce9d4fe965c923ec08b20a8291a095a8314
Jenna Sylvia <jenna.sylvia@berkeley.edu>
2020-01-31 01:11:58 +0000

    strikethrough final

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -3,14 +3,16 @@ $(function() {
     // of the task and produces an HTML representation using
     // <li> tags
     function taskHtml(task) {
-      var checkedStatus = task.done ? "checked" : "";
-      var liClass = task.done ? "completed" : "";
-      var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '">' +
-        " data-id='" + task.id + "'" +
-        checkedStatus +
-        '><label>' +
-         task.title +
-         '</label></div></li>';
+    var checkedStatus = task.done ? "checked" : "";
+    var liClass = task.done ? "completed" : "";
+    var liElement = '<li class="' + liClass + '">' +
+    '<div class="view"><input class="toggle" type="checkbox"' +
+      " data-id='" + task.id + "'" +
+      checkedStatus +
+      '><label>' +
+       task.title +
+       '</label></div></li>';
+
 
       return liElement;
     }
